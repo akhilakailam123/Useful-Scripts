8c6f5c1fe28bf87d8d9e6d0b65be90f48d08ad45
akhilakailam123 <kailamakhila@gmail.com>
2025-04-09 18:16:19 +0530

    xml files added

---
+++ pyspark-scripts/extract-xml-data.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+
+spark = SparkSession \
+    .builder \
+    .appName("Test") \
+    .config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.13.0") \
+    .getOrCreate()
+
+df2_schema = StructType([
+    StructField("_id", StringType(), True),
+    StructField("author", StringType(), True),
+    StructField("title", StringType(), True),
+    StructField("genre", StringType(), True),
+    StructField("price", StringType(), True),
+    StructField("publish_date", StringType(), True),
+    StructField("description", StringType(), True),
+])
+df2 = (spark.read.format('xml')
+       .options(rowTag='book')
+       .option("attributePrefix", "_")
+       .schema(df2_schema)
+       .load('input_data/xml_data.xml')
+       )
+df2.printSchema()
+df2.show()
+
+'''Run file with spark-submit if issues occures
+spark-submit --packages com.databricks:spark-xml_2.12:0.13.0 get-xml-data.py'''
